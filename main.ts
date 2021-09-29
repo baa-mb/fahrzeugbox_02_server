cdd000bbf66291f53d7c4cfdfa19176634e84902
baa <34665120+baa-mb@users.noreply.github.com>
2021-09-29 19:46:57 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -30,6 +30,8 @@ radio.onReceivedString(function (receivedString) {
                 EventBusSource.MICROBIT_ID_BUTTON_B,
                 EventBusValue.MICROBIT_BUTTON_EVT_CLICK
                 )
+            } else {
+                lenkung(parseFloat(receivedString))
             }
         }
     }
@@ -44,6 +46,11 @@ input.onButtonPressed(Button.B, function () {
         . . . . .
         `)
 })
+function lenkung (num: number) {
+    winkel = Math.map(num, -1023, 1023, 0, 180)
+    pins.servoWritePin(AnalogPin.P1, winkel)
+}
+let winkel = 0
 basic.showString("DLPL")
 basic.showIcon(IconNames.Diamond)
 radio.setGroup(54)
